939d279795ea3f9905e677eb77897a5cb7b579fb
OlesiaViazankina <lisena.cmex@rambler.ru>
2019-02-13 00:24:14 +0300

    Диплом, добавила изменения

--- src/sample/AudioPlaylist.java
+++ src/sample/AudioPlaylist.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.*;
+import javafx.beans.InvalidationListener;
 import javafx.beans.value.*;
 import javafx.collections.*;
 import javafx.event.*;
@@ -12,6 +13,7 @@ import javafx.scene.image.*;
 import javafx.scene.layout.*;
 import javafx.scene.media.*;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Callback;
 import javafx.util.Duration;
 
@@ -35,7 +37,7 @@ public class AudioPlaylist extends Application {
     public static void main(String[] args) throws Exception { launch(args); }
 
     public void start(final Stage stage) throws Exception {
-        stage.setTitle("Audio Player");
+        stage.setTitle("My Audio Player");
 
         // определить исходный каталог для списка воспроизведения (либо первый аргумент программы, либо значение по умолчанию).
         final List<String> params = getParameters().getRaw();
@@ -67,6 +69,17 @@ public class AudioPlaylist extends Application {
         final MediaView mediaView = new MediaView(players.get(0));
         final Button skip = new Button("Skip");
         final Button play = new Button("Pause");
+        final Button stop = new Button ("Stop");
+
+        Label volumeLabel = new Label("Volume:");
+        final Slider volumeSlider = new Slider (0,100,50);
+        volumeSlider.setMaxWidth(Double.MAX_VALUE);
+        volumeSlider.valueProperty().addListener(ov -> {
+            final MediaPlayer mp = mediaView.getMediaPlayer ();
+            if (volumeSlider.isPressed ()) {
+                mp.setVolume(volumeSlider.getValue() / 100.0);
+            }
+        });
 
         // воспроизводим каждый аудиофайл по очереди.
         for (int i = 0; i < players.size(); i++) {
@@ -90,10 +103,15 @@ public class AudioPlaylist extends Application {
                 curPlayer.currentTimeProperty().removeListener(progressChangeListener);
                 curPlayer.getMedia().getMetadata().removeListener(metadataChangeListener);
                 curPlayer.stop();
-
                 MediaPlayer nextPlayer = players.get((players.indexOf(curPlayer) + 1) % players.size());
                 mediaView.setMediaPlayer(nextPlayer);
-                nextPlayer.play();
+                if ("Pause".equals(play.getText()) & skip.isArmed ()) {
+                    nextPlayer.play ();
+                    nextPlayer.setVolume (volumeSlider.getValue ()/100.0);
+                } else if("Play".equals(play.getText()) & skip.isArmed ()){
+                    nextPlayer.pause ();
+                    nextPlayer.setVolume (volumeSlider.getValue ()/100.0);
+                }
             }
         });
 
@@ -109,7 +127,17 @@ public class AudioPlaylist extends Application {
                 }
             }
         });
-
+        stop.setOnAction (new EventHandler<ActionEvent> () {
+            @Override public void handle(ActionEvent event) {
+                final MediaPlayer curPlayer = mediaView.getMediaPlayer();
+                curPlayer.currentTimeProperty().removeListener(progressChangeListener);
+                curPlayer.getMedia().getMetadata().removeListener(metadataChangeListener);
+                curPlayer.stop();
+                if ("Pause".equals(play.getText()) & stop.isArmed ()) {
+                    play.setText("Play");
+                }
+            }
+        });
         // отображаем название текущего трека
         mediaView.mediaPlayerProperty().addListener(new ChangeListener<MediaPlayer>() {
             @Override public void changed(ObservableValue<? extends MediaPlayer> observableValue, MediaPlayer oldPlayer, MediaPlayer newPlayer) {
@@ -141,7 +169,7 @@ public class AudioPlaylist extends Application {
         nameColumn.setCellValueFactory(new MapValueFactory<>(COLUMN_NAME));
         valueColumn.setCellValueFactory(new MapValueFactory<>(COLUMN_VALUE));
 
-        metadataTable.setEditable(true);//делает таблицу доступной для редактирования, не вижу изменений?
+        metadataTable.setEditable(true);//делает таблицу доступной для редактирования
         metadataTable.getSelectionModel().setCellSelectionEnabled(true);//дает возможность выбрать ячейку, по умолчанию стоит выбор строки
         metadataTable.getColumns().setAll(nameColumn, valueColumn); //добавляем столбцы в таблицу
         valueColumn.setCellFactory(new Callback<TableColumn<Map, Object>, TableCell<Map, Object>>() {
@@ -181,16 +209,23 @@ public class AudioPlaylist extends Application {
         //позиционирование элементов
         //стек
         final StackPane layout = new StackPane();
-        layout.setStyle("-fx-background-color: cornsilk; -fx-font-size: 20; -fx-padding: 20; -fx-alignment: center;");
+        layout.setStyle("-fx-background-color: paleturquoise; -fx-font-size: 20; -fx-padding: 20; -fx-alignment: center;");
         //горизонтальный столбец - прогресс бар
         final HBox progressBar = new HBox(10);
         progressBar.setAlignment(Pos.CENTER);
-        progressBar.getChildren().setAll(play, skip, progress);
-        //один вертикальный столбец
+        progressBar.getChildren().setAll(play, stop, skip, progress);
+
+        final HBox volumeBar = new HBox (10);
+        volumeBar.setAlignment (Pos.CENTER);
+        volumeBar.setPrefWidth (Double.MAX_VALUE);
+        volumeBar.getChildren ().setAll (volumeLabel, volumeSlider);
+
+        //вертикальный столбец
         final VBox content = new VBox(10);
         content.getChildren().setAll(
                 currentlyPlaying,
                 progressBar,
+                volumeBar,
                 metadataTable
         );
 
@@ -199,7 +234,8 @@ public class AudioPlaylist extends Application {
                 content
         );
         progress.setMaxWidth(Double.MAX_VALUE);
-        HBox.setHgrow(progress, Priority.ALWAYS);
+        HBox.setHgrow (progress, Priority.ALWAYS);
+        HBox.setHgrow(volumeSlider, Priority.ALWAYS);
         VBox.setVgrow(metadataTable, Priority.ALWAYS);
 
         Scene scene = new Scene(layout, 600, 400);
